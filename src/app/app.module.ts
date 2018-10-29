8664fe40908949493ffbcc836e848a9d53a8593d
Hansin Susatya <hansinsusatya@gmail.com>
2018-10-29 21:29:42 +0700

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ZippyComponent } from './zippy/zippy.component';
+import { TodosComponent } from './todos/todos.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent, TodosComponent, ZippyComponent ],
+	imports: [ BrowserModule, BrowserAnimationsModule ],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
